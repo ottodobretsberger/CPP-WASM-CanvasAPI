3719cad68a2da97d2d5f292d949badef4600da82
Dr. Otto Dobretsberger <OttoD@eps-software.com>
2024-04-10 17:36:29 -0500

    many small rectangles

--- draw.cpp
+++ draw.cpp
@@ -38,28 +38,44 @@ extern "C" {
 
     EMSCRIPTEN_KEEPALIVE void drawRandomRectangles(int numRectangles, int canvasWidth, int canvasHeight) {
         srand(time(NULL)); // Seed the random number generator
-
-        for(int i = 0; i < 30; i++)
+        
+        for(int i = 0; i < canvasWidth; i+=4)
         {
-            for (int i = 0; i < numRectangles; ++i) {
-                    int x = rand() % canvasWidth;     // Random x-coordinate
-                    int y = rand() % canvasHeight;    // Random y-coordinate
-                    int width = rand() % 50 + 10 ;     // Random width between 10 and 70
-                    int height = rand() % 50 + 10;    // Random height between 10 and 70
+            for(int j = 0; j < canvasHeight; j+=4)
+            {
+                int width = 3;
+                int height = 3;
+                char color[20];
+                 color[0] = 'r'; color[1] = 'g'; color[2] = 'b'; color[3] = '(';
+                 color[4] = '0'; color[5] = ','; color[6] = '0'; color[7] = ',';
+                 color[8] = '0'; color[9] = ')'; color[10] = '\0';
 
-                    // Generate a random color in RGB format
-                    char color[20]; // Sufficient size for the color string
-                    color[0] = 'r'; color[1] = 'g'; color[2] = 'b'; color[3] = '(';
-                    intToString(rand() % 256, color + 4); // Red component
-                    color[6] = ',';
-                    intToString(rand() % 256, color + 7); // Green component
-                    color[9] = ',';
-                    intToString(rand() % 256, color + 10); // Blue component
-                    color[12] = ')';
-                    color[13] = '\0';
-                    
-                    drawRectangleOnCanvas(x, y, width, height, color);
+                 drawRectangleOnCanvas(i, j, width, height, color);
             }
         }
+
+
+        // for(int i = 0; i < 30; i++)
+        // {
+        //     for (int i = 0; i < numRectangles; ++i) {
+        //             int x = rand() % canvasWidth;     // Random x-coordinate
+        //             int y = rand() % canvasHeight;    // Random y-coordinate
+        //             int width = rand() % 50 + 10 ;     // Random width between 10 and 70
+        //             int height = rand() % 50 + 10;    // Random height between 10 and 70
+
+        //             // Generate a random color in RGB format
+        //             char color[20]; // Sufficient size for the color string
+        //             color[0] = 'r'; color[1] = 'g'; color[2] = 'b'; color[3] = '(';
+        //             intToString(rand() % 256, color + 4); // Red component
+        //             color[6] = ',';
+        //             intToString(rand() % 256, color + 7); // Green component
+        //             color[9] = ',';
+        //             intToString(rand() % 256, color + 10); // Blue component
+        //             color[12] = ')';
+        //             color[13] = '\0';
+                    
+        //             drawRectangleOnCanvas(x, y, width, height, color);
+        //     }
+        // }
     }
 }
